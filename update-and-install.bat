c7198ab534184ac49a35d9de542f52b69ce2b173
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2024-03-14 20:54:59 +0700

    Delete update-and-install.bat

--- update-and-install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Automatic run as admin...
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-
-rem Set environment variable
-set CURRENT_PATH=%~dp0
-setx CURRENT_PATH "%CURRENT_PATH%" /M
